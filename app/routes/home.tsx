4eb635ec2902833fb86ccc6785ce852b41fbb7c5
Venkat <venkat415@Venkats-MacBook-Air.local>
2025-11-25 02:52:55 +0530

    Implement Home and Score Circle

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,33 @@
+import { resumes } from "~/constants";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "resumind.ai" },
-    { name: "description", content: "Welcome to React Router!" },
+    { name: "description", content: "Smart feedback for ur dream job !!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track Your Applications & Resume Scores</h1>
+          <h2>View your submissions and get instant AI-powered insights.</h2>
+        </div>
+
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
